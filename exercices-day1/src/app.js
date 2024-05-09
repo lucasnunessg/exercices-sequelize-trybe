b32a7022347d02e07ff2cf6e6773c2ccf157f089
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-05-09 09:05:18 -0300

    criando as rotas no app

--- exercices-day1/src/app.js
+++ exercices-day1/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { bookController } = require('./controllers/books.controller');
 
 const app = express()
 
 app.use(express.json());
+app.get('/books', bookController.getAll);
+app.get('/books/:id', bookController.getById);
+app.delete('/books/:id', bookController.deleteBook);
+app.post('/books', bookController.addBook);
+app.put('/books/:id', bookController.updateBook);
